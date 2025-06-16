52be3b8fc05299d3deb6febdb9f218c82e3c4e22
Devansh <dev_perks@outlook.com>
2025-06-16 13:50:39 +0530

    Fix typo in LandingPage component and adjust layout for better presentation

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,11 +4,16 @@ export default function LandingPage() {
   return (
     <div className="w-full h-screen bg-zinc-900">
       <div className="textstrucuture pt-40 px-20">
-        {["We Create", "Eye Opening", "Presentations"].map((text, index) => (
+        {["We Create", "Eye-Opening", "Presentations"].map((text, index) => (
           <div className="masker" key={index}>
-            <h1 className="uppercase font-founders text-[138.667px] leading-[104px] font-semibold">
-              {text}
-            </h1>
+            <div className="w-fit flex items-center overflow-hidden">
+              {index === 1 && (
+                <div className="w-[9vw] h-[5.9vw] bg-blue-400 relative top-[0.5vw] rounded-md mr-[1.305vw]"></div>
+              )}
+              <h1 className="uppercase font-founders text-[9vw] leading-[6.8vw] font-semibold">
+                {text}
+              </h1>
+            </div>
           </div>
         ))}
       </div>
