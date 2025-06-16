f789c4f01e272dd7f1d23f04111a4775ae692edd
Devansh <dev_perks@outlook.com>
2025-06-16 20:11:24 +0530

    Add About component and update App layout to include it

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] rounded-tl-2xl rounded-tr-2xl text-black">
+      <h1 className="max-w-[1226.67px] font-neuemontreal text-[3.611vw] leading-none">
+        Ochi is a strategic presentation agency for forward-thinking businesses
+        that need to{" "}
+        <span className="underline">
+          raise funds, sell products, explain complex ideas,
+        </span>{" "}
+        and{" "}
+        <span className="underline">hire great peo-ple.</span>
+      </h1>
+    </div>
+  );
+}
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,12 +4,12 @@ import React from "react";
 export default function Marquee() {
   return (
     <div className="w-full py-10 bg-[#004D43] rounded-tl-2xl rounded-tr-2xl">
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden">
+      <div className="text border-t-2 border-b-2 border-[#4D837C] flex whitespace-nowrap overflow-hidden">
         <motion.h1
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
           transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
-          className="uppercase font-founders text-[28.472vw] font-normal leading-none -mb-10 -mt-22"
+          className="uppercase font-founders text-[28.472vw] font-normal leading-none -mb-[2.2vw] -mt-[5.9vw]"
         >
           We are Ochi&nbsp;
         </motion.h1>
@@ -17,7 +17,7 @@ export default function Marquee() {
           initial={{ x: 0 }}
           animate={{ x: "-100%" }}
           transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
-          className="uppercase font-founders text-[28.472vw] font-normal leading-none -mb-10 -mt-22"
+          className="uppercase font-founders text-[28.472vw] font-normal leading-none -mb-[2.2vw] -mt-[5.9vw]"
         >
           We are Ochi&nbsp;
         </motion.h1>
