3c714eef7f1bc2879d0f3cca66399d1a7836beef
Devansh <dev_perks@outlook.com>
2025-06-18 13:36:50 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,22 @@ export default function About() {
         <span className="underline">
           raise funds, sell products, explain complex ideas,
         </span>{" "}
-        and{" "}
-        <span className="underline">hire great peo-ple.</span>
+        and <span className="underline">hire great peo-ple.</span>
       </h1>
+      <div className="w-full border-t-[1px] border-[#A5BC57] mt-20 pt-10 flex gap-5">
+        <div className="w-1/2 font-neuemontreal">
+          <h1 className="text-[3.61vw] leading-none">Our approach:</h1>
+          <button className="flex gap-10 items-center uppercase px-7 py-6 bg-zinc-900 rounded-full text-white mt-6 font-neuemontreal leading-none text-[.975vw]">
+            Read More
+            <div className="w-2.5 h-2.5 bg-zinc-100 rounded-full"></div>
+          </button>
+        </div>
+        <div className="w-1/2 h-[70vh] bg-cyan-400 rounded-lg">
+          <img src="src/assets/images/about-photo.jpg" alt="About" 
+           className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+      </div>
     </div>
   );
 }
