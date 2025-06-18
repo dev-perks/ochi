cb105c535fdc5dde03bf77a1efdf9e66471614fb
Devansh <dev_perks@outlook.com>
2025-06-18 16:02:18 +0530

    Fix formatting by adding space around subtraction operator in Eyes component

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -12,7 +12,7 @@ export default function Eyes() {
       let deltaY = mouseY - window.innerHeight / 2;
 
       var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-      setRotate(angle-180);
+      setRotate(angle - 180);
     });
   });
 
