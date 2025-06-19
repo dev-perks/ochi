e36d27724be41a1cd4b0c4f21342ac5b4726f4e5
Devansh <dev_perks@outlook.com>
2025-06-19 15:30:27 +0530

    Add Footer component and integrate LocomotiveScroll for enhanced scrolling effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import About from "./components/About";
 import Cards from "./components/Cards";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
+import LocomotiveScroll from "locomotive-scroll";
 
 export default function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar />
@@ -16,6 +20,7 @@ export default function App() {
       <Eyes />
       <Featured />
       <Cards />
+      <Footer />
     </div>
-  )
+  );
 }
